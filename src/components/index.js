56e9725bebba21b1ac281a91c2430a7362ee88c8
Gaëlle De Nas De Tourris <gaelledndt@outlook.com>
2023-03-06 19:01:54 +0100

    Initialisation

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas/index.js';
+import Hero from "./Hero.jsx";
+import Navbar from "./Navbar.jsx";
+import About from "./About.jsx";
+import Tech from "./Tech.jsx";
+import Experience from "./Experience.jsx";
+import Works from "./Works.jsx";
+import Feedbacks from "./Feedbacks.jsx";
+import Contact from "./Contact.jsx";
+import CanvasLoader from "./Loader.jsx";
+
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+};
